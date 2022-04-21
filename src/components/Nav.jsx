18ca721c6fcc311311d17cacc68a8287a653a32b
Skyz03 <aakibdiscoo@gmail.com>
2022-04-21 14:35:05 +0545

    Material UI implementation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import logo from "../images/logo.svg";
+import { AppBar, Typography } from "@mui/material";
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <div className="logo">
-        <a href="/" className="logo-link">
-          <img src={logo} alt="Logo" />
-        </a>
-      </div>
+    <>
+      <React.Fragment>
+        <AppBar position="sticky" color="hsl(167, 40%, 24%)">
+          <Typography>
+            <img src={logo} alt="Logo" />
+          </Typography>
+        </AppBar>
+      </React.Fragment>
+      {/* <nav className="nav">
+        <div className="logo">
+          <a href="/" className="logo-link"></a>
+        </div>
 
-      <div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div>
+          <ul className="nav-links">
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav> */}
+    </>
   );
 };
 
