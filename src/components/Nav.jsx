e1a2b60b8b6f44f9b37f31e2e2066736bfdc5e5f
Skyz03 <aakibdiscoo@gmail.com>
2022-04-21 14:45:53 +0545

    Updates in the navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,37 +4,11 @@ import { AppBar, Typography } from "@mui/material";
 
 const Nav = () => {
   return (
-    <>
-      <React.Fragment>
-        <AppBar position="sticky" color="hsl(167, 40%, 24%)">
-          <Typography>
-            <img src={logo} alt="Logo" />
-          </Typography>
-        </AppBar>
-      </React.Fragment>
-      {/* <nav className="nav">
-        <div className="logo">
-          <a href="/" className="logo-link"></a>
-        </div>
-
-        <div>
-          <ul className="nav-links">
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Projects</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
-    </>
+    <AppBar position="sticky">
+      <Typography>
+        <img src={logo} alt="Logo" />
+      </Typography>
+    </AppBar>
   );
 };
 
