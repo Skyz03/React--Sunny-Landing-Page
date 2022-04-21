aa6fe9221e06631ba8e436ef25edd375cae0149a
Skyz03 <aakibdiscoo@gmail.com>
2022-04-21 16:56:01 +0545

    Updates in the nav with toggle and progress with theme mode

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,48 @@
 import React from "react";
 import logo from "../images/logo.svg";
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Tabs, Tab, Button, Switch } from "@mui/material";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 const Nav = () => {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
   return (
-    <AppBar position="sticky">
-      <Typography>
-        <img src={logo} alt="Logo" />
-      </Typography>
-    </AppBar>
+    <React.Fragment>
+      <AppBar position="sticky" sx={{ background: "#3ebfff" }}>
+        <Toolbar>
+          <img src={logo} alt="Logo" />
+
+          <Tabs
+            sx={{ marginLeft: "auto" }}
+            textColor="#f0f"
+            value={0}
+            indicatorColor="#fff"
+          >
+            <Tab className="nav-link" label="About" />
+            <Tab className="nav-link" label="Services" />
+            <Tab className="nav-link" label="Projects" />
+          </Tabs>
+
+          <Button
+            className="contact-btn"
+            sx={{
+              background: "#fff",
+              color: "#000",
+              borderRadius: "20px",
+              fontWeight: "bold",
+            }}
+            variant="contained"
+            color="primary"
+          >
+            Contact
+          </Button>
+
+          <Switch />
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 };
 
