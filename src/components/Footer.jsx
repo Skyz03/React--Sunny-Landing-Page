be0bd81f6094acdbb992c153d1e37db024de3c12
Skyz03 <aakibdiscoo@gmail.com>
2022-04-24 17:22:41 +0545

    Icons imported

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
 
 const Footer = () => {
   return (
@@ -19,6 +23,13 @@ const Footer = () => {
               <a href="#">Projects</a>
             </li>
           </ul>
+
+          <div className="icons">
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon />
+            <PinterestIcon />
+          </div>
         </div>
       </Container>
     </section>
