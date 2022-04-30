c09c27624856011155c86ba65af36e7bc8541f0c
Skyz03 <aakibdiscoo@gmail.com>
2022-04-30 16:51:18 +0545

    Updates in the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,11 +14,40 @@ import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Drawer from "@mui/material/Drawer";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
 
 const Nav = () => {
-  const [value, setValue] = React.useState(0);
+  const menuItems = [
+    {
+      text: "Home",
+      icon: "home",
+      link: "/",
+    },
+    {
+      text: "About",
+      icon: "info",
+      link: "/about",
+    },
+  ];
 
   const theme = useTheme();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = React.useState(0);
+
   const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleChange = (event, newValue) => {
@@ -31,11 +60,24 @@ const Nav = () => {
         <Toolbar>
           <img src={logo} alt="Logo" />
 
+          <IconButton color="inherit" edge="end" onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+
           {matches ? (
-            <Drawer variant="permanent" anchor="right">
+            <Drawer open={open} variant="permanent" anchor="right">
               <div>
                 <Typography variant="h6">Menu</Typography>
               </div>
+
+              <List>
+                {menuItems.map((item, index) => (
+                  <ListItem key={item.text} button>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItem>
+                ))}
+              </List>
             </Drawer>
           ) : (
             <>
